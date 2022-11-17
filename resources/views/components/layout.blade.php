c5c951779ca81eb983c41f6592d3a043bd2d44c6
Charles <charlesmariga37@gmail.com>
2022-11-17 16:42:19 +0300

    feat: extracting a newsletter service

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,19 @@
 <!doctype html>
 
-<title>Laravel From Scratch Blog</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<html>
+
+<head>
+    <title>Laravel From Scratch Blog</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <style>
+        html {
+            scroll-behavior: smooth
+        }
+    </style>
+</head>
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
@@ -28,7 +37,7 @@
                     <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
                 @endauth
 
-                <a href="#"
+                <a href="#newsletter"
                     class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
@@ -37,22 +46,26 @@
 
         {{ $slot }}
 
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16 mx-6">
+        <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16 mx-6">
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
-            <div class="mt-10">
+            <div class="mt-10 flex flex-col items-center space-y-4">
                 <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
-                    <form method="POST" action="#" class="lg:flex text-sm">
+                    <form method="POST" action="newsletter" class="lg:flex text-sm" autocomplete="off">
+                        @csrf
                         <div class="lg:py-3 lg:px-5 flex items-center">
                             <label for="email" class="hidden lg:inline-block">
                                 <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                             </label>
 
-                            <input id="email" type="text" placeholder="Your email address"
-                                class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                            <input id="email" type="email" placeholder="Your email address" name="email"
+                                class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none"
+                                :value="{{ old('email') }}">
+
                         </div>
 
                         <button type="submit"
@@ -61,9 +74,14 @@
                         </button>
                     </form>
                 </div>
+                @error('email')
+                    <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
             </div>
         </footer>
 
         <x-flash />
     </section>
 </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('newsletter', NewsletterController::class);
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
