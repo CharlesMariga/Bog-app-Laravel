f7c870ed9c2ecc4391fb697be92b308efb394237
Charles <charlesmariga37@gmail.com>
2022-11-19 10:48:15 +0300

    feat: validating and storing posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
-            <form method="POST" action="/admin/posts">
+    <section class="max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish new post
+        </h1>
+        <x-panel>
+            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="title">Title</label>
@@ -18,6 +21,16 @@
                         value="{{ old('slug') }}" required>
                     @error('slug')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="title">Thumbnail</label>
+                    <input type="file" class="border border-gray-400 p-2 w-full" name="thumbnail" id="thumbnail"
+                        accept="image/*">
+                    required>
+                    @error('thumbnail')
+                        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
                     @enderror
                 </div>
 
