bfe611d266c2f94d80aca85f1976c78b8ee48089
Charles <charlesmariga37@gmail.com>
2022-11-19 14:00:46 +0300

    feat: extending the admin layout

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,10 +1,10 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}" />
 
     <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}"
-        class="border border-gray-400 p-2 w-full" value="{{ old($name) }}" required>
+        class="border border-gray-200 p-2 w-full rounded" value="{{ old($name) }}" {{ $attributes }} required>
 
     <x-form.error name="{{ $name }}" />
 </x-form.field>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,34 +1,28 @@
 <x-layout>
-    <section class="max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4">
-            Publish new post
-        </h1>
-        <x-panel>
-            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
-                @csrf
-                <x-form.input name="title" />
-                <x-form.input name="slug" />
-                <x-form.input name="thumbnail" type="file" />
-                <x-form.textarea name="excerpt" />
-                <x-form.textarea name="body" />
+    <x-setting heading="Publish new post">
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
+            <x-form.input name="thumbnail" type="file" />
+            <x-form.textarea name="excerpt" />
+            <x-form.textarea name="body" />
 
-                <x-form.field>
-                    <x-form.label name="category" />
-                    <select name="category_id" id="category_id" class="border border-gray-400 p-2 w-full" required>
-                        <option value="">Select One...</option>
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}"
-                                {{ old('category_id') == $category->id ? 'selected' : '' }}>
-                                {{ ucwords($category->name) }}</option>
-                        @endforeach
-                    </select>
-                    @error('category_id')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </x-form.field>
+            <x-form.field>
+                <x-form.label name="category" />
+                <select name="category_id" id="category_id" class="border border-gray-200 rounded p-2 w-full" required>
+                    <option value="">Select One...</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                            {{ ucwords($category->name) }}</option>
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                @enderror
+            </x-form.field>
 
-                <x-form.button>Publish</x-form.button>
-            </form>
-        </x-panel>
-    </section>
+            <x-form.button>Publish</x-form.button>
+        </form>
+    </x-setting>
 </x-layout>
